c8e4739a43d7cb8298f8f56dd89719562969fbc3
Andrii <andry.zhidkikh@gmail.com>
2024-09-24 08:40:04 +0300

    [TASK] - make framework

---
+++ app/abstractClasses.ts
@@ -0,0 +1,25 @@
+import type { Page } from '@playwright/test';
+import { step } from '../misc/reporters/step';
+
+export abstract class PageHolder { 
+    constructor(protected page: Page) { }
+}
+export abstract class Component extends PageHolder {
+    
+}
+
+export abstract class AppPage extends Component {
+    /**
+     * Path to the page can be relative to the baseUrl defined in playwright.config.ts
+     * or absolute (on your own risk)
+     */
+    public abstract pagePath: string;
+
+    /**
+     * Opens the page in the browser and expectLoaded should pass
+     */
+    @step()
+    async open(path?: string) {
+        await this.page.goto(path ?? this.pagePath);
+    }
+}
